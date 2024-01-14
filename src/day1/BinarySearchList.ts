26d0238c02ac638a7b70c823a5a014af28cd8b7e
Sarthik Gupta <sarthikg@icloud.com>
2024-01-14 16:34:59 +0530

    feat: initialised binary-search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let left = 0
+    let right = haystack.length
+
+    while (left < right) {
+        const center = Math.floor(left + (right - left) / 2)
+
+        if (haystack[center] === needle) {
+            return true
+        }
+        else if (haystack[center] > needle) {
+            right = center
+        } else {
+            left = center + 1
+        }
+    }
+
+    return false
 
 }
