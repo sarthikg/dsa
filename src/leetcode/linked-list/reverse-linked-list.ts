486b7123a44a2f0f34b43764cf0fd473c176705a
Sarthik Gupta <sarthikg@icloud.com>
2024-04-08 19:14:24 +0530

    feat(leetcode): reverse-linked-list

---
+++ src/leetcode/linked-list/reverse-linked-list.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode<string> | null): ListNode<string> | null {
+  // Initialise pointers for prevNode & currNode
+  let prevNode = null;
+  let currNode = head;
+
+  // While current node exists
+  while (currNode) {
+    // Store pointer to the next node
+    let nextNode = currNode.next;
+    // Update next pointer for the current node
+    currNode.next = prevNode;
+    // Update the previous node pointer
+    prevNode = currNode;
+    // Update the current node pointer
+    currNode = nextNode;
+  }
+
+  // At the end of the loop, prevNode will have reference to start of the linked list
+  return prevNode;
+}
